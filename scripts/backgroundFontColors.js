c36652edf86e1f9912503f72723397d9b179af55
Charlie Homewood <charliehomewood99@gmail.com>
2023-07-07 23:17:57 +0100

    initial commit

---
+++ scripts/backgroundFontColors.js
@@ -0,0 +1,134 @@
+//////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////// CHANGE BACKGROUND AND FONT COLOR OF CURRENT PAGE //////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// DEFINE CURRENT URL //
+var currentURL = window.location.href;
+
+// DEFINE QUERYSELECTORALL VARIABLES //
+var pageBody = document.querySelectorAll("#default-body");
+var pageSiteurl = document.querySelectorAll("#header-siteurl");
+var pageNavlink = document.querySelectorAll("#header-nav-link");
+var pageH1 = document.querySelectorAll("#header-title");
+var blogItem = document.querySelectorAll("#blog-item");
+var projectsPageItem = document.querySelectorAll("#projectpage-item");
+var projectPost = document.querySelectorAll("#projects-post");
+var footerSocialLink = document.querySelectorAll("#footer-social-link");
+var footerCopyright = document.querySelectorAll("#footer-copyright");
+var aboutContentLink = document.querySelectorAll("#about-content-link");
+
+// DEFINE PAGE COLORS //
+
+// PROJECTS //
+var projectsBackgroundColor = "background-color: rgba(1, 41, 41, 255)";
+var projectsFontColor = "color: rgba(202, 69, 70, 255)";
+
+// ABOUT //
+var aboutBackgroundColor = "background-color: rgba(19, 74, 79, 255)";
+var aboutFontColor = "color: rgba(14, 255, 204, 255)";
+
+// BLOG //
+var blogBackgroundColor = "background-color: rgba(69, 39, 117, 255)";
+var blogFontColor = "color: rgba(202, 187, 255, 255)";
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+if (currentURL.includes("projects")) {
+
+  // PROJECTS PAGE //
+
+  pageBody.forEach((pageBody) => {
+    pageBody.style.cssText = `${projectsBackgroundColor}; ${projectsFontColor}`;
+  });
+
+  pageSiteurl.forEach((pageSiteurl) => {
+    pageSiteurl.style.cssText = `${projectsBackgroundColor}; ${projectsFontColor}`;
+  });
+
+  pageNavlink.forEach((pageNavlink) => {
+    pageNavlink.style.cssText = `${projectsBackgroundColor}; ${projectsFontColor}`;
+  });
+
+  pageH1.forEach((pageH1) => {
+    pageH1.style.cssText = `${projectsBackgroundColor}; ${projectsFontColor}`;
+  });
+
+  projectsPageItem.forEach((projectsPageItem) => {
+    projectsPageItem.style.cssText = `${projectsBackgroundColor}; ${projectsFontColor}`;
+  });
+
+  projectPost.forEach((projectPost) => {
+    projectPost.style.cssText = `${projectsBackgroundColor}; ${projectsFontColor}`;
+  });
+
+  footerSocialLink.forEach((footerSocialLink) => {
+    footerSocialLink.style.cssText = `${projectsBackgroundColor}; ${projectsFontColor}`;
+  });
+
+  footerCopyright.forEach((footerCopyright) => {
+    footerCopyright.style.cssText = `${projectsBackgroundColor}; ${projectsFontColor}`;
+  });
+
+} else if (currentURL.includes("about")) {
+
+  // ABOUT PAGE //  
+
+  pageBody.forEach((pageBody) => {
+    pageBody.style.cssText = `${aboutBackgroundColor}; ${aboutFontColor}`;
+  });
+
+  pageSiteurl.forEach((pageSiteurl) => {
+    pageSiteurl.style.cssText = `${aboutBackgroundColor}; ${aboutFontColor}`;
+  });
+
+  pageNavlink.forEach((pageNavlink) => {
+    pageNavlink.style.cssText = `${aboutBackgroundColor}; ${aboutFontColor}`;
+  });
+
+  pageH1.forEach((pageH1) => {
+    pageH1.style.cssText = `${aboutBackgroundColor}; ${aboutFontColor}`;
+  });
+
+  footerSocialLink.forEach((footerSocialLink) => {
+    footerSocialLink.style.cssText = `${aboutBackgroundColor}; ${aboutFontColor}`;
+  });
+
+  footerCopyright.forEach((footerCopyright) => {
+    footerCopyright.style.cssText = `${aboutBackgroundColor}; ${aboutFontColor}`;
+  });
+
+  aboutContentLink.forEach((aboutContentLink) => {
+    aboutContentLink.style.cssText = `${aboutBackgroundColor}; ${aboutFontColor}`;
+  });
+
+} else if (currentURL.includes("blog")) {
+
+  pageBody.forEach((pageBody) => {
+    pageBody.style.cssText = `${blogBackgroundColor}; ${blogFontColor}`;
+  });
+
+  pageSiteurl.forEach((pageSiteurl) => {
+    pageSiteurl.style.cssText = `${blogBackgroundColor}; ${blogFontColor}`;
+  });
+
+  pageNavlink.forEach((pageNavlink) => {
+    pageNavlink.style.cssText = `${blogBackgroundColor}; ${blogFontColor}`;
+  });
+
+  pageH1.forEach((pageH1) => {
+    pageH1.style.cssText = `${blogBackgroundColor}; ${blogFontColor}`;
+  });
+
+  blogItem.forEach((blogItem) => {
+    blogItem.style.cssText = `${blogBackgroundColor}; ${blogFontColor}`;
+  });
+
+  footerSocialLink.forEach((footerSocialLink) => {
+    footerSocialLink.style.cssText = `${blogBackgroundColor}; ${blogFontColor}`;
+  });
+
+  footerCopyright.forEach((footerCopyright) => {
+    footerCopyright.style.cssText = `${blogBackgroundColor}; ${blogFontColor}`;
+  });
+
+}
---
+++ scripts/homeHoverColors.js
@@ -0,0 +1,103 @@
+var homeNavA = document.querySelectorAll("#home-nav-a");
+var homeBody = document.querySelectorAll("#home-body");
+var homeH1Link = document.querySelectorAll("#home-h1-link");
+
+var transition = "transition-duration: 1.5s";
+
+homeNavA.forEach(function(homeNavA) {
+
+  var text = homeNavA.textContent;
+
+  homeNavA.addEventListener("mouseover", function() {
+    if (text === "Projects") {
+
+      homeNavA.style.cssText = `${projectsFontColor}; ${transition}`;
+
+      homeBody.forEach((homeBody) => {
+        homeBody.style.cssText = `${projectsBackgroundColor}; ${transition}`;
+      });
+
+      homeH1Link.forEach((homeH1Link) => {
+        homeH1Link.style.cssText = `${projectsFontColor}; ${transition}`;
+      });
+
+      footerSocialLink.forEach((footerSocialLink) => {
+        footerSocialLink.style.cssText = `${projectsFontColor}; ${transition}`;
+      });
+
+      footerCopyright.forEach((footerCopyright) => {
+        footerCopyright.style.cssText = `${projectsFontColor}; ${transition}`;
+      });
+
+    } else if (text === "About") {
+      
+      homeNavA.style.cssText = `${aboutFontColor}; ${transition}`;
+
+      homeBody.forEach((homeBody) => {
+        homeBody.style.cssText = `${aboutBackgroundColor}; ${transition}`;
+      });
+
+      homeH1Link.forEach((homeH1Link) => {
+        homeH1Link.style.cssText = `${aboutFontColor}; ${transition}`;
+      });
+
+      footerSocialLink.forEach((footerSocialLink) => {
+        footerSocialLink.style.cssText = `${aboutFontColor}; ${transition}`;
+      });
+
+      footerCopyright.forEach((footerCopyright) => {
+        footerCopyright.style.cssText = `${aboutFontColor}; ${transition}`;
+      });
+
+    }
+
+    else if (text === "Blog") {
+      
+      homeNavA.style.cssText = `${blogFontColor}; ${transition}`;
+
+      homeBody.forEach((homeBody) => {
+        homeBody.style.cssText = `${blogBackgroundColor}; ${transition}`;
+      });
+
+      homeH1Link.forEach((homeH1Link) => {
+        homeH1Link.style.cssText = `${blogFontColor}; ${transition}`;
+      });
+
+      footerSocialLink.forEach((footerSocialLink) => {
+        footerSocialLink.style.cssText = `${blogFontColor}; ${transition}`;
+      });
+
+      footerCopyright.forEach((footerCopyright) => {
+        footerCopyright.style.cssText = `${blogFontColor}; ${transition}`;
+      });
+
+    }
+
+  });
+
+  homeNavA.addEventListener("mouseout", function() {
+
+    homeNavA.style.color = "";
+
+    homeBody.forEach((homeBody) => {
+      homeBody.style.backgroundColor = "";
+    });
+
+    homeH1Link.forEach((homeH1Link) => {
+      homeH1Link.style.color = "";
+    });
+
+    footerSocialLink.forEach((footerSocialLink) => {
+      footerSocialLink.style.color = "";
+      footerSocialLink.style.transitionDuration = "";
+      footerSocialLink.style.cssText = `${"transition: 0.4s ease"}`;
+    });
+
+    footerCopyright.forEach((footerCopyright) => {
+      footerCopyright.style.color = "";
+    });
+  
+  });
+
+});
+
